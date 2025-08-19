7ef423d98955fd91b9e733fc596e20ba61763626
Guilherme <tairentsuki@gmail.com>
2025-08-19 09:57:04 -0300

    Criado nome dos arquivos

--- src/desafio2/main.java
+++ src/desafio2/main.java
@@ -1,4 +1,18 @@
 package desafio2;
 
 public class main {
+    public static void main(String[] args) {
+
+        final int QNT_ARQUIVOS = 10;
+        String[] nomeArquivos = new String[QNT_ARQUIVOS];
+
+        for(int i = 0; i < QNT_ARQUIVOS; i++){
+            nomeArquivos[i] = "dna-"+i+".txt";
+        }
+
+        for(int i = 0; i < QNT_ARQUIVOS; i++){
+            System.out.println(nomeArquivos[i]);
+        }
+
+    }
 }
